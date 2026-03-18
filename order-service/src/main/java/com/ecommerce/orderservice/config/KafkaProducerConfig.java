371e88d16f899744b8eb110d981c76a0b031ab70
Vikas Dandge <vikas.dandge07@gmail.com>
2026-03-18 23:49:17 +0530

    removed Docker setting

--- order-service/src/main/java/com/ecommerce/orderservice/config/KafkaProducerConfig.java
+++ order-service/src/main/java/com/ecommerce/orderservice/config/KafkaProducerConfig.java
@@ -25,7 +25,8 @@ public class KafkaProducerConfig {
 
         Map<String, Object> props = new HashMap<>();
 
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
